c1da577d4c9d589d765f0cd3932413b9814666f4
Labasse <labasse@gmail.com>
2025-03-25 21:02:30 +0100

    Organisation - Code de CekikilaWasm

---
+++ CekikilaAuto/CekikilaAuto.Client/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace CekikilaWasm.Models
+{
+    public class Item
+    {
+        public int Id { get; init; }
+        public required string Name { get; set; }
+        public string? Description
+        {
+            get; init;
+        }
+
+        public required string[] Tags;
+    }
+}
--- CekikilaAuto/CekikilaAuto.Client/Program.cs
+++ CekikilaAuto/CekikilaAuto.Client/Program.cs
@@ -1,7 +1,9 @@
+using CekikilaWasm.Models;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddSingleton<DataService>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
