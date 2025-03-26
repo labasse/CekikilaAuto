1159e78f915599b22f9c69e31887ec57ff7a18c0
Labasse <labasse@gmail.com>
2025-03-26 16:28:02 +0100

    Intégration - Formulaire simple

--- CekikilaLib/ItemDto.cs
+++ CekikilaLib/ItemDto.cs
@@ -1,9 +1,15 @@
-﻿namespace CekikilaLib
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CekikilaLib
 {
     public class ItemDto
     {
         public int Id { get; set; }
+
+        [Required, StringLength(50, MinimumLength=2)]
         public required string Name { get; set; }
+
+        [Display(Name="Description")]
         public string? Description
         {
             get; set;
